08f33933a7462a5606059713fa999595c1e720f4
b10t <b10t.rus@gmail.com>
2022-08-19 16:14:57 +0700

    Uploading questions and answers to redis.

--- redis_tools.py
+++ redis_tools.py
@@ -1,15 +1,20 @@
 import logging
 import redis
 
+from environs import Env
+
 
 def create_redis():
     """
     Create a new redis instance.
     """
-    redis_host = ''
-    redis_port = ''
-    redis_username = ''
-    redis_password = ''
+    env = Env()
+    env.read_env()
+
+    redis_host = env.str('REDIS_HOST', 'localhost')
+    redis_port = env.str('REDIS_PORT', 6379)
+    redis_username = env.str('REDIS_USERNAME', '')
+    redis_password = env.str('REDIS_PASSWORD', '')
 
     return redis.Redis(
         host=redis_host,
@@ -19,20 +24,41 @@ def create_redis():
     )
 
 
-def delete_all_qa():
-    """Удаляет все вопросы и ответы с БД."""
-    qa_keys = r.keys(pattern='QA_*')
-    r.delete(*qa_keys)
+def set_json_value(key, value):
+    """Установить значение JSON по ключу."""
+    return bd_redis.json().set(key, '$', value)
 
 
-def delete_all_user_data():
-    """Удаляет все данные пользователей."""
-    user_keys = r.keys(pattern='TG_*')
-    r.delete(*user_keys)
-
-    user_keys = r.keys(pattern='VK_*')
-    r.delete(*user_keys)
+def get_json_value(key):
+    """Получить значение JSON по ключу."""
+    return bd_redis.json().get(key)
 
 
+def set_value(key, value):
+    """Установить значение по ключу."""
+    return bd_redis.set(key, value)
 
-r = redis.Redis()
+
+def get_value(key):
+    """Получить значение по ключу."""
+    return bd_redis.get(key)
+
+
+def fetch_keys(pattern='*'):
+    """Получает список ключей по шаблону."""
+    return bd_redis.keys(pattern)
+
+
+def delete_key(key):
+    """Удаляет запись по ключу."""
+    bd_redis.delete(key)
+
+
+def delete_all_keys(pattern='*'):
+    """Удаляет все все ключи по шаблону."""
+    if keys := bd_redis.keys(pattern=pattern):
+        bd_redis.delete(*keys)
+
+
+logger = logging.getLogger('quiz-bot')
+bd_redis = create_redis()
---
+++ uploading_quiz_data.py
@@ -0,0 +1,93 @@
+import glob
+import hashlib
+import logging
+import os
+import re
+
+import redis_tools
+
+logger = logging.getLogger('quiz-bot')
+
+
+def get_qa_key(question):
+    """Возвращает ключ для хранения вопросов в базе данных."""
+    return 'QA_%s' % hashlib.md5(question.encode('utf-8')).hexdigest()
+
+
+def save_qa_to_redis(questions_answers):
+    """Сохраняет вопросы и ответы в БД redis."""
+    question_reg = r'^Вопрос \d+:'
+    answer_reg = r'^Ответ:'
+
+    question_text = ''
+    answer_text = ''
+
+    for qa_text in questions_answers:
+        if re.match(question_reg, qa_text):
+            question_text = re.sub(question_reg, '', qa_text).strip()
+
+        if re.match(answer_reg, qa_text):
+            answer_text = re.sub(answer_reg, '', qa_text).strip()
+
+            if question_text:
+                data = {
+                    'question': question_text,
+                    'answer': answer_text
+                }
+
+                redis_tools.set_json_value(
+                    get_qa_key(question_text),
+                    data
+                )
+
+
+def get_qa_from_file(quiz_content):
+    """Получает из файла вопросы и ответы викторины."""
+    questions_answers = []
+
+    current_qa = []
+    index = -1
+
+    while (True):
+        try:
+            index += 1
+
+            if quiz_content[index] == '\n':
+                questions_answers.append(''.join(current_qa))
+                current_qa = []
+                continue
+
+            current_qa.append(quiz_content[index])
+
+        except IndexError:
+            break
+
+    return questions_answers
+
+
+def processing_quizzes_files(folder='quizzes_files'):
+    """Обрабатывает файлы с викторинами из папки."""
+    if not os.path.isdir(folder):
+        logger.error(f'The folder with quizzes was not found: {folder}')
+        return
+
+    for quiz_filename in glob.glob(os.path.join(folder, '*.txt')):
+        logger.info(f'Found quiz file: {quiz_filename}')
+
+        with open(quiz_filename, 'r', encoding='KOI8-R') as quiz_file:
+            questions_answers = get_qa_from_file(
+                quiz_file.readlines()
+            )
+
+            save_qa_to_redis(questions_answers)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO,
+    )
+
+    redis_tools.delete_all_keys('QA_*')
+
+    processing_quizzes_files()
